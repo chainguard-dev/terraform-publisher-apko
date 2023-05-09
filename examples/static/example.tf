f6ee24636fa826169779d8d633d6571e1766d086
Matt Moore <mattmoor@chainguard.dev>
2023-05-08 20:50:31 -0700

    Feature: Add config attestations
    :gift: Attest the locked configuration files on images
    
    /kind feature

--- examples/static/example.tf
+++ examples/static/example.tf
@@ -32,7 +32,7 @@ module "image" {
   config = file("${path.module}/static.yaml")
 }
 
-data "cosign_verify" "image-signature" {
+data "cosign_verify" "image-signatures" {
   for_each = module.image.archs
   image    = module.image.arch_to_image[each.key]
 
@@ -60,7 +60,7 @@ data "cosign_verify" "image-signature" {
   })
 }
 
-data "cosign_verify" "index-sbom" {
+data "cosign_verify" "sbom-attestations" {
   for_each = module.image.archs
   image    = module.image.arch_to_image[each.key]
 
@@ -98,6 +98,46 @@ data "cosign_verify" "index-sbom" {
     }
   })
 }
+
+data "cosign_verify" "config-attestations" {
+  for_each = module.image.archs
+  image    = module.image.arch_to_image[each.key]
+
+  policy = jsonencode({
+    apiVersion = "policy.sigstore.dev/v1beta1"
+    kind       = "ClusterImagePolicy"
+    metadata = {
+      name = "config-attestation"
+    }
+    spec = {
+      images = [{ glob = "**" }]
+      authorities = [{
+        keyless = {
+          url = "https://fulcio.sigstore.dev"
+          identities = [{
+            issuer  = "https://token.actions.githubusercontent.com"
+            subject = "https://github.com/chainguard-dev/terraform-publisher-apko/.github/workflows/test.yaml@refs/heads/main"
+          }]
+        }
+        ctlog = {
+          url = "https://rekor.sigstore.dev"
+        }
+        attestations = [
+          {
+            name = "config-att"
+            predicateType = "https://apko.dev/image-configuration"
+            policy = {
+              type = "cue"
+              # TODO(mattmoor): Add more meaningful checks.
+              data = "predicateType: \"https://apko.dev/image-configuration\""
+            }
+          },
+        ]
+      }]
+    }
+  })
+}
+
 
 output "image_ref" {
   value = module.image.image_ref
--- main.tf
+++ main.tf
@@ -23,16 +23,29 @@ resource "apko_build" "this" {
   config = data.apko_config.this.config
 }
 
+resource "cosign_sign" "signature" {
+  image = apko_build.this.image_ref
+}
+
+locals {
+  archs = toset(concat(data.apko_config.this.config.archs, length(data.apko_config.this.config.archs) > 1 ? ["index"] : []))
+}
+
 # Create SBOM attestations for each architecture.
 resource "cosign_attest" "sboms" {
-  for_each = toset(concat(data.apko_config.this.config.archs,
-  length(data.apko_config.this.config.archs) > 1 ? ["index"] : []))
+  for_each = local.archs
 
   image          = apko_build.this.sboms[each.key].digest
   predicate_type = apko_build.this.sboms[each.key].predicate_type
   predicate      = apko_build.this.sboms[each.key].predicate
 }
 
-resource "cosign_sign" "signature" {
-  image = apko_build.this.image_ref
+# Create attestations for each architecture holding the "locked"
+# configuration used to perform the build.
+resource "cosign_attest" "apko-configuration" {
+  for_each = local.archs
+
+  image          = apko_build.this.sboms[each.key].digest
+  predicate_type = "https://apko.dev/image-configuration"
+  predicate      = jsonencode(data.apko_config.this.config)
 }
