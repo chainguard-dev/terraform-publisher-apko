5b340f3468b742fa76bb4076632882e5c0ff0656
Matt Moore <mattmoor@chainguard.dev>
2023-05-09 14:01:22 -0700

    Incorporate breaking changes from 0.8.2 (#8)

--- examples/static/example.tf
+++ examples/static/example.tf
@@ -19,10 +19,10 @@ variable "target_repository" {
 }
 
 provider "apko" {
-  repositories = ["https://packages.wolfi.dev/os"]
-  keyring      = ["https://packages.wolfi.dev/os/wolfi-signing.rsa.pub"]
-  archs        = ["x86_64", "aarch64"]
-  packages     = ["wolfi-baselayout"]
+  extra_repositories = ["https://packages.wolfi.dev/os"]
+  extra_keyring      = ["https://packages.wolfi.dev/os/wolfi-signing.rsa.pub"]
+  default_archs      = ["x86_64", "aarch64"]
+  extra_packages     = ["wolfi-baselayout"]
 }
 
 module "image" {
